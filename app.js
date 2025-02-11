1ad36174dc808e3b162e88ac1f509be1a8d3ede5
aayushranmale <aayushranmale53@gmail.com>
2025-02-11 13:32:39 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-let scrollContainer=document.querySelector(".gallery");
-let backBtn=document.getElementById("backBtn");
-let nextBtn=document.getElementById("nextBtn");
-
-nextBtn.addEventListener("click",()=>{
-    scrollContainer.style.scrollBehavior ="smooth";
-    scrollContainer.scrollLeft += 900;
-});
-
-backBtn.addEventListener("click",()=>{
-    scrollContainer.style.scrollBehavior ="smooth";
-    scrollContainer.scrollLeft -= 900;
+function appendValue(value) {
+    document.getElementById('display').value += value;
+}
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+function calculate() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (error) {
+        alert('Invalid Calculation');
+        clearDisplay();
+    }
+}
+document.addEventListener('keydown', function(event) {
+    const key = event.key;
+    if (/[0-9.+\-*/]/.test(key)) {
+        appendValue(key);
+    } else if (key === 'Enter') {
+        calculate();
+    } else if (key === 'Escape') {
+        clearDisplay();
+    }
 });
